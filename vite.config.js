aef9e0af618b71198b1fabfd2060158bb719623a
Reorx <novoreorx@gmail.com>
2022-09-05 14:52:48 +0800

    add china painting colors page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-/** @type {import('vite').UserConfig} */
-export default {
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
   base: "",
   build: {
     assetsDir: ".",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        painting: resolve(__dirname, 'painting/index.html'),
+      },
+    },
   },
-}
+})
